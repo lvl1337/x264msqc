c76a9ac7878046d3d3f557f44cc58d06c7eb65a6
lvl1337 <152334179+lvl1337@users.noreply.github.com>
2023-11-29 23:32:32 +0300

    AviSynth changes + VapourSynth support from aMod

--- input/input.h
+++ input/input.h
@@ -48,6 +48,7 @@ typedef struct
     int output_csp; /* convert to this csp, if applicable */
     int output_range; /* user desired output range */
     int input_range; /* user override input range */
+    char *frameserver_lib_path; /* path to external frameserver library */
 } cli_input_opt_t;
 
 /* properties of the source given by the demuxer */
@@ -102,6 +103,7 @@ typedef struct
 extern const cli_input_t raw_input;
 extern const cli_input_t y4m_input;
 extern const cli_input_t avs_input;
+extern const cli_input_t vpy_input;
 extern const cli_input_t thread_8_input;
 extern const cli_input_t thread_10_input;
 extern const cli_input_t lavf_input;
